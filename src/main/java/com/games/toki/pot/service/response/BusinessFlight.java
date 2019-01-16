54ce3bf46dcdc721026957fe7326da7b65d06e62
Manish Mundra <mmundra@DELC02QM9MAG8WN.sea.corp.expecn.com>
2019-01-17 05:19:24 +0530

    Updated the details in cheap & business flight, going to be returned from provided endpoints

--- src/main/java/com/games/toki/pot/service/response/BusinessFlight.java
+++ src/main/java/com/games/toki/pot/service/response/BusinessFlight.java
@@ -1,5 +1,22 @@
 package com.games.toki.pot.service.response;
 
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class BusinessFlight {
+	private String id;
+	private String departureFromArrivalTo;
+
+	private String departureTime;
+	private String arrivalTime;
+
+	public BusinessFlight(String id, String departureFromArrivalTo, String departureTime, String arrivalTime) {
+		this.id = id;
+		this.departureFromArrivalTo = departureFromArrivalTo;
+		this.departureTime = departureTime;
+		this.arrivalTime = arrivalTime;
+	}
 }
--- src/main/java/com/games/toki/pot/service/response/CheapFlight.java
+++ src/main/java/com/games/toki/pot/service/response/CheapFlight.java
@@ -1,5 +1,23 @@
 package com.games.toki.pot.service.response;
 
-public class CheapFlight {
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
+public class CheapFlight {
+	private String id;
+	private String departureFrom;
+	private String arrivalTo;
+
+	private String departureTime;
+	private String arrivalTime;
+
+	public CheapFlight(String id, String departureFrom, String arrivalTo, String departureTime, String arrivalTime) {
+		this.id = id;
+		this.departureFrom = departureFrom;
+		this.arrivalTo = arrivalTo;
+		this.departureTime = departureTime;
+		this.arrivalTime = arrivalTime;
+	}
 }
